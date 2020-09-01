d18c825ec65ec9c8092f9b55acceb17cd576fd8c
Sean Ni <nixiang915@hotmail.com>
2020-09-01 01:57:23 +0000

    added post handling using body parser

--- express_server.js
+++ express_server.js
@@ -23,6 +23,11 @@ app.get("/urls", (req, res) => {
   res.render('urls_index', templateVars);
 });
 
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
